0c6faf80add759933d3e1be68ea3db24a20c6f45
Fedoseev Alex <sanekbuster@mail.ru>
2020-01-14 09:31:12 +0300

    wip

--- algorithms/blurred.cpp
+++ algorithms/blurred.cpp
@@ -1,50 +1,83 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
 #include <string>
+#include <algorithm>
 
-std::pair<long, long> extract_numbers(std::string, int);
+std::vector<long> extract_numbers(std::string, int);
+int picture(std::vector<std::vector<long>> ranges);
 
 int main(void){
   std::string buf;
 
   std::cin.width(99);
-  std::cin >> buf;
+  std::getline(std::cin, buf);
 
   long rows = 0;
   rows = std::stol(buf);
 
-  std::vector<std::pair<long, long>> non_blurred = {{0, 0}};
-  std::string raw_nums = {0};
+  std::vector<std::vector<long>> non_blurred;
+  std::string raw_nums;
   for(long r = 0; r < rows; r++){
     std::getline(std::cin, raw_nums);
     non_blurred.push_back(extract_numbers(raw_nums, 2));
   }
-  std::cout << "array = ";
-  for(auto item: non_blurred){
-    std::cout << "{" << item.first << " " << item.second << "}" ;
-  }
+
+  std::cout << "answer: " << picture(non_blurred) << std::endl;
 
   return EXIT_SUCCESS;
 }
 
 
-std::pair<long, long> extract_numbers(std::string str, int nums_count){
-  std::pair<long, long> result = {0, 0};
+std::vector<long> extract_numbers(std::string str, int nums_count){
+  std::vector<long> result;
+  std::size_t start_pos = 0, end_pos = 0;
 
   for(int i = 0; i < nums_count; i++){
-    std::size_t start_pos = str.find_first_of("0123456789");
+    start_pos = str.find_first_of("0123456789");
 
     if(start_pos == std::string::npos){
       return result;
     }
 
-    std::size_t end_pos = str.find_first_not_of("0123456789", start_pos);
-    std::string to_convert = str.substr(start_pos,
-                                        std::string::npos == end_pos ? end_pos: end_pos - start_pos);
-    std::cout << std::stol(to_convert) << " found" << std::endl;
-    str = str.substr(end_pos);
+    end_pos = str.find_first_not_of("0123456789", start_pos);
+    std::string to_convert = str.substr(start_pos);
+    result.push_back(std::stol(to_convert));
+    if(end_pos != std::string::npos){
+      str = str.substr(end_pos);
+    }
   }
+  return result;
+}
+
+int picture(std::vector<std::vector<long>> ranges){
+  int **array = new int*[ranges.size()];
+  for(unsigned long i = 0; i < ranges.size(); i++){
+    array[i] = new int[ranges.size()]{};
+  }
+
+  int result = 0;
+  for(int i = 0; i < (int) ranges.size(); i++){
+    int *row = array[i];
+
+    for(long j = ranges[i][0]; j < ranges[i][1]; j++){
+      int upper = 0, diag = 0, left = 0;
+
+      if(i - 1 >= 0 && j - 1 >= 0){
+        diag = array[i - 1][j - 1];
+        upper = array[i - 1][j];
+        left = array[i][j - 1];
+      } else if(i - 1 >= 0) {
+        upper = array[i - 1][j];
+      } else {
+      }
+      result = result > row[j] ? result: row[j];
+    }
+  }
+
+  for(unsigned long i = 0; i < ranges.size(); i++){
+    delete[] array[i];
+  }
+  delete[] array;
 
   return result;
 }
