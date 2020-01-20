86aaa5d27a4745f6c68281e18cdbc76902c14e15
0b5erver <0b5erver@protonmail.com>
2020-01-21 00:23:02 +0300

    Added rust parsing

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::dbg;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut input: String = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Unable to read from 'stdin'!");
+
+    let size: u16 = input.trim().parse().expect("Error during conversion to u16");
+    dbg!(size);
+
+    let mut ranges: Vec<(u16, u16)> = Vec::new();
+    for _ in 0..size {
+        input.clear();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Unable to read range from stdin");
+
+        let parts: Vec<&str> = input.split(' ').collect();
+        let range: (u16, u16) = (dbg!(parts[0]).trim().parse::<u16>().expect("Error during start conversion"),
+                                 dbg!(parts[1]).trim().parse::<u16>().expect("Error during end conversion"));
+        ranges.push(range);
+        dbg!(range);
+    }
 }
