a1df8e960e2d4cbe4576ef26383154158eda36fa
FedoseevAlex <fedoseevalex@inbox.ru>
2020-03-04 14:14:24 +0300

    Added stepik task solution

---
+++ algorithms/misc/stepic_task1.hs
@@ -0,0 +1,88 @@
+data Bit = Zero | One deriving (Show, Eq)
+data Sign = Minus | Plus deriving (Show, Eq)
+data Z = Z Sign [Bit] deriving (Show, Eq)
+
+z_to_int :: Z -> Int
+z_to_int (Z Minus bits) = (- bit_to_int bits)
+z_to_int (Z Plus bits) = bit_to_int bits
+
+bit_to_int :: [Bit] -> Int
+bit_to_int = foldr (\x a -> 2 * a + convert x) 0 
+    where
+        convert :: Bit -> Int
+        convert Zero = 0
+        convert One = 1
+
+int_to_bit :: Int -> [Bit]
+int_to_bit 0 = [Zero]
+int_to_bit 1 = [One]
+int_to_bit number = remainder': int_to_bit quotent 
+    where
+        (quotent, remainder) = divMod number 2
+        remainder' = if remainder == 1 then One else Zero
+
+int_to_z :: Int -> Z
+int_to_z number
+    | number < 0 = Z Minus $ (int_to_bit . abs) number
+    | number > 0 = Z Plus $ int_to_bit number
+    | otherwise = Z Plus []
+
+add :: Z -> Z -> Z
+add a b = int_to_z $ (z_to_int a) + (z_to_int b)
+
+mul :: Z -> Z -> Z
+mul a b = int_to_z $ (z_to_int a) * (z_to_int b)
+
+-- Tests section
+emptyZ = Z Plus []
+test001 = (add (Z Plus []) (Z Plus [])) == Z Plus []
+test002 = (add (Z Plus []) (Z Plus [One])) == Z Plus [One]
+test003 = (add (Z Plus []) (Z Minus [One])) == Z Minus [One]
+
+test011 = (add (Z Plus [Zero, One, One]) (Z Plus [One])) == Z Plus [One, One, One]
+test012 = (add (Z Plus [Zero, One, One]) (Z Plus [Zero, One])) == Z Plus [Zero, Zero, Zero, One]
+test013 = (add (Z Plus [Zero, One, One]) (Z Plus [Zero, One, One])) == Z Plus [Zero, Zero, One, One]
+
+test021 = (add (Z Minus [Zero, One, One]) (Z Minus [One])) == Z Minus [One, One, One]
+test022 = (add (Z Minus [Zero, One, One]) (Z Minus [Zero, One])) == Z Minus [Zero, Zero, Zero, One]
+test023 = (add (Z Minus [Zero, One, One]) (Z Minus [Zero, One, One])) == Z Minus [Zero, Zero, One, One]
+
+test031 = (add (Z Minus [Zero, One, One]) (Z Plus [One])) == Z Minus [One, Zero, One]
+test032 = (add (Z Minus [Zero, One, One]) (Z Plus [Zero, One])) == Z Minus [Zero, Zero, One]
+test033 = (add (Z Minus [Zero, One, One]) (Z Plus [Zero, One, One])) == Z Plus []
+
+test041 = (add (Z Plus [Zero, One, One]) (Z Minus [One])) == Z Plus [One, Zero, One]
+test042 = (add (Z Plus [Zero, One, One]) (Z Minus [Zero, One])) == Z Plus [Zero, Zero, One]
+test043 = (add (Z Plus [Zero, One, One]) (Z Minus [Zero, One, One])) == Z Plus []
+
+test051 = (add (Z Plus [One]) (Z Minus [One])) == Z Plus []
+test052 = (add (Z Plus [One]) (Z Minus [One, One])) == Z Minus [Zero, One]
+test053 = (add (Z Plus [One]) (Z Minus [Zero, One])) == Z Minus [One]
+test054 = (add (Z Plus [One]) (Z Minus [Zero, Zero, Zero, One])) == Z Minus [One, One, One]
+test055 = (add (Z Plus [One]) (Z Minus [Zero, One, Zero, One])) == Z Minus [One, Zero, Zero, One]
+test056 = (add (Z Plus [Zero, One]) (Z Minus [Zero, One, One])) == Z Minus [Zero, Zero, One]
+test057 = (add (Z Plus [Zero, One]) (Z Minus [Zero, Zero, One])) == Z Minus [Zero, One]
+test058 = (add (Z Plus [One, Zero, One]) (Z Minus [Zero, One, Zero, One])) == Z Minus [One, Zero, One]
+
+
+test101 = (mul (Z Plus []) (Z Plus [])) == emptyZ
+test102 = (mul (Z Plus []) (Z Plus [One])) == emptyZ
+test103 = (mul (Z Plus []) (Z Minus [One])) == emptyZ
+test104 = (mul (Z Plus [One]) (Z Plus [])) == emptyZ
+test105 = (mul (Z Minus [One]) (Z Plus [])) == emptyZ
+
+test111 = (mul (Z Plus [One]) (Z Plus [One])) == Z Plus [One]
+test112 = (mul (Z Minus [One]) (Z Plus [One])) == Z Minus [One]
+test113 = (mul (Z Plus [One]) (Z Minus [One])) == Z Minus [One]
+test114 = (mul (Z Minus [One]) (Z Minus [One])) == Z Plus [One]
+
+test121 = (mul (Z Plus [One]) (Z Plus [Zero, One])) == Z Plus [Zero, One]
+test122 = (mul (Z Plus [Zero, Zero, One]) (Z Plus [Zero, Zero, One])) == Z Plus [Zero, Zero, Zero, Zero, One]
+
+test131 = (mul (Z Plus [One, Zero, One, Zero, One]) (Z Plus [One, One, One])) == Z Plus [One, One, Zero, Zero, One, Zero, Zero, One]
+
+
+testAdd = test001 && test002 && test003 && test011 && test012 && test013 && test021 && test022 && test023 && test031 && test032 && test033 && test041 && test042 && test043 && test051 && test052 && test053 && test054 && test055 && test056 && test057 && test058
+testMul = test101 && test102 && test103 && test104 && test105 && test111 && test112 && test113 && test114 && test121 && test122 && test131
+
+testAll = testAdd && testMul
