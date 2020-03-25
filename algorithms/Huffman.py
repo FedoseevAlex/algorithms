da404f17eaee97dba0f53826050c54994861185c
Fedoseev Aleksandr <fedoseev@selectel.ru>
2020-03-25 21:14:26 +0300

    Finished huffman algorithm

--- algorithms/Huffman.py
+++ algorithms/Huffman.py
@@ -1,111 +1,78 @@
 from collections import Counter
+from operator import itemgetter
+from pprint import pprint
 
 
-class Node:
+def prepare_answer_encode() -> str:
     """
-    Класс ноды бинарного дерева.
-    """
+    Функция подготовки ответа для тестов на создание кода Хаффмана.
 
-    def __init__(self):
-        """
-        В конструкторе создаются два поля класса для левого и правого
-        ребенка соответственно.
-        """
-        self.left = None
-        self.right = None
-
-    @property
-    def weight(self):
-        """
-        Свойство расчета веса данной вершины.
-        Для каждого ребенка проверяется его тип, в зависимости от
-        типа вычисляется вес ребенка. Результирующий вес ноды равен
-        сумме весов детей.
-        """
-        if isinstance(self.left, tuple):
-            left_weight = self.left[1]
-        else:
-            left_weight = self.left.weight() if self.left is not None else 0
-
-        if isinstance(self.right, tuple):
-            right_weight = self.right[1]
-        else:
-            right_weight = self.right.weight() if self.right is not None else 0
-
-        return left_weight + right_weight
-
-
-def build_tree(input_string: str) -> Node:
-    """
-    Функция построения дерева частот для дальнейшего создания кода Хаффмана.
-
-    :param input_string: Входная строка для которой строится дерево
-    :return: Корень дерева частот
-    """
-    counter = sorted(Counter(input_string).items(),
-                     key=lambda x: x[1])
-
-    # В цикле происходит жадный шаг алгоритма.
-    # На каждой итерации в дерево будут объединены символы
-    # c наименьшей частотой. Шаги будем продолжать до тех пор
-    # пока в массиве не останется только корень дерева.
-    while True:
-        root = Node()
-        root.left = counter.pop(0)
-        root.right = counter.pop(0) if counter else None
-        counter.append((root, root.weight))
-        if len(counter) == 1:
-            break
-        counter.sort(key=lambda x: x[1])
-    return counter[0]
-
-
-def make_code(struct, prefix: str = "") -> list:
-    """
-    Функция, создающая код Хаффмана для переданного дерева
-    частот символов.
-    Эта функция рекурсивно обойдет переданное дерево в
-    глубину и вернет список пар: (символ, код символа)
-
-    :param struct: Дерево частот символов строки
-    :param prefix: Параметр, накапливающий код символа
-    :return: Список пар
-    """
-    answer = []
-    tree, _ = struct
-
-    if isinstance(tree, str):
-        answer.append((tree, prefix))
-    elif isinstance(tree, Node):
-        if tree.left is not None:
-            answer.extend(make_code(tree.left, prefix + "0"))
-        if tree.right is not None:
-            answer.extend(make_code(tree.right, prefix + "1"))
-    return answer
-
-
-def prepare_answer(input_string: str) -> str:
-    """
-    Функция подготовки ответа для тестов
-
-    :param input_string: Строка для которой будет построен код
-    Хаффмана
     :return: Строка с ответом на задание
     """
+    input_string = input()
     answer = list()
-    tree = build_tree(input_string)
-    codes = dict(make_code(tree))
-    letters_num = len(codes)
-    coded_sequence = [codes[letter] for letter in input_string]
-    coded_string = "".join(coded_sequence)
-    answer.append(f"{letters_num} {len(coded_string)}")
-    for letter, code in codes.items():
-        answer.append(f"{letter}: {code}")
-    answer.append(coded_string)
-
+    code = make_code(input_string)
+    translation_table = str.maketrans(code)
+    encoded_string = input_string.translate(translation_table)
+    answer.append(f'{len(code.keys())} {len(encoded_string)}')
+    for letter, coding in code.items():
+        answer.append(f'{letter}: {coding}')
+    answer.append(encoded_string)
     return "\n".join(answer)
 
 
+def make_code(input_string: str) -> dict:
+    """
+    Построение дерева по заданной строке
+
+    :param input_string: Строка для которой будет построено дерево
+    частот
+    :return: Список из символа, его частоты и списка для кода Хаффмана
+    """
+    counts = list(Counter(input_string).items())
+    codes = {key[0]: '' for key in counts}
+
+    while True:
+        counts.sort(key=itemgetter(1))
+
+        left_letter, left_weight = counts.pop(0)
+        for letter in left_letter:
+            codes[letter] = '0' + codes[letter]
+
+        right_letter, right_weight = counts.pop(0) if counts else ('', 0)
+        for letter in right_letter:
+            codes[letter] = '1' + codes[letter]
+        counts.append((left_letter + right_letter, left_weight + right_weight))
+
+        if len(counts) == 1:
+            break
+    return codes
+
+
+def prepare_answer_decode():
+    """
+    Функция для разбора строк с кодом Хаффмана для символов
+    и вывода результата для тестов.
+    """
+    symbols_num, _ = input().split()
+    symbols_num = int(symbols_num)
+
+    codes = dict()
+    for _ in range(symbols_num):
+        symbol, code = input().split(': ')
+        codes[code] = symbol
+    encoded_string = input()
+
+    current = ''
+    result = ''
+    for symbol in encoded_string:
+        current += symbol
+        if current not in codes:
+            continue
+        result += codes[current]
+        current = ''
+    return result
+
+
 if __name__ == "__main__":
-    inp = input()
-    print(prepare_answer(inp))
+    print(prepare_answer_decode())
