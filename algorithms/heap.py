36bd5591a314b4188b3dc2bc718a5317f23e0505
Fedoseev Aleksandr <fedoseev@selectel.ru>
2020-04-01 22:31:37 +0300

    Possibly working variant

--- algorithms/heap.py
+++ algorithms/heap.py
@@ -1,54 +1,53 @@
 from typing import List
 
 
-def sift_up(heap: List):
-    bubble = len(heap) - 1
-    parent = bubble // 2
-    while bubble > 0:
-        if heap[bubble] > heap[parent]:
-            heap[bubble], heap[parent] = heap[parent], heap[bubble]
-        parent, bubble = parent // 2, parent
+def sift_up(heap: List, pos: int = None):
+    if pos is None:
+        pos = len(heap) - 1
+    current, parent = pos, pos // 2
+
+    while current > 0:
+        if heap[current] > heap[parent]:
+            heap[current], heap[parent] = heap[parent], heap[current]
+        else:
+            break
+        current, parent = parent, parent // 2
+
+
+def sift_down(heap: List, pos: int = 0):
+    left = pos * 2 + 1
+    right = pos * 2 + 2
+    if right < len(heap):
+        max_child = left if heap[left] > heap[right] else right
+    elif left < len(heap):
+        max_child = left
+    else:
+        return
+
+    if heap[pos] < heap[max_child]:
+        heap[pos], heap[max_child] = heap[max_child], heap[pos]
+
+    sift_down(heap, max_child)
 
 
 def insert(heap: List, number: int):
     heap.append(number)
-    sift_up(heap)
+    sift_up(heap, len(heap) - 1)
 
 
-def sift_down(heap: List):
-    bubble = 0
-    left, right = bubble * 2 + 1, bubble * 2 + 2
+def heapify(array: List):
 
-    while bubble < len(heap):
-        if right < len(heap):
-            max_child = left if heap[left] > heap[right] else right
-        elif left < len(heap):
-            max_child = left
-        else:
-            break
-
-        if heap[max_child] > heap[bubble]:
-            heap[max_child], heap[bubble] = heap[bubble], heap[max_child]
-        bubble, left, right = max_child, max_child * 2 + 1, max_child * 2 + 2
+    for idx in reversed(range(len(array))):
+        sift_down(array, idx)
 
 
-def pop_max(heap: List) -> int:
-    root = heap.pop(0) if heap else None
+def pop(heap: List):
+    root = heap.pop(0)
     if heap:
-        heap.append(heap.pop())
+        heap.insert(0, heap.pop())
         sift_down(heap)
     return root
 
 
 if __name__ == "__main__":
-    ops_num = int(input())
-    ops = list()
-    for _ in range(ops_num):
-        ops.append(input().split())
-
-    arr = list()
-    for op in ops:
-        if len(op) > 1:
-            insert(arr, int(op[1]))
-        else:
-            print(pop_max(arr))
+    pass
--- tests/heap_test.py
+++ tests/heap_test.py
@@ -1,77 +1,22 @@
-import heapq
-from operator import neg
-from random import shuffle
-
 import pytest
 
 from algorithms import heap
 
-
-def test_base():
-    arr = []
-    heap.insert(arr, 200)
-    heap.insert(arr, 10)
-    assert heap.pop_max(arr) == 200
-    heap.insert(arr, 5)
-    heap.insert(arr, 500)
-    assert heap.pop_max(arr) == 500
-
-
-def test_case_1():
-    arr = []
-    heap.insert(arr, 200)
-    heap.insert(arr, 10)
-    heap.insert(arr, 5)
-    heap.insert(arr, 500)
-    heap.pop_max(arr)
-    heap.pop_max(arr)
-    heap.pop_max(arr)
-    heap.pop_max(arr)
+testdata = [
+    list(range(5)),
+    list(range(15)),
+    list(range(50)),
+    list(range(150)),
+    list(range(1000)),
+    list(range(100000)),
+]
 
 
-def test_case_2():
-    arr = []
-    heap.insert(arr, 10)
-    heap.insert(arr, 10)
-    heap.insert(arr, 8)
-    heap.pop_max(arr)
-    assert arr == [10, 8]
+@pytest.mark.parametrize("numbers", testdata)
+def test_simple(numbers):
+    heap_arr = numbers.copy()
+    heap.heapify(heap_arr)
 
-
-def test_case_3():
-    arr = []
-    heap.insert(arr, 2)
-    heap.insert(arr, 3)
-    heap.insert(arr, 15)
-    heap.insert(arr, 18)
-    heap.insert(arr, 12)
-    heap.pop_max(arr)
-    heap.pop_max(arr)
-    heap.pop_max(arr)
-    assert arr == [3, 2]
-
-
-def test_case_4():
-    arr = []
-    heap.insert(arr, 4)
-    heap.insert(arr, 3)
-    heap.insert(arr, 0)
-    heap.pop_max(arr)
-    assert arr == [3, 0]
-
-def test_case_5():
-    arr = []
-    heap.insert(arr, 53)
-    heap.insert(arr, 7)
-    heap.insert(arr, 22)
-    heap.insert(arr, 6)
-    heap.insert(arr, 5)
-    heap.insert(arr, 21)
-    heap.insert(arr, 20)
-    assert heap.pop_max(arr) == 53
-    assert heap.pop_max(arr) == 22
-    assert heap.pop_max(arr) == 21
-    assert heap.pop_max(arr) == 20
-    assert heap.pop_max(arr) == 7
-    assert heap.pop_max(arr) == 6
-    assert heap.pop_max(arr) == 5
+    for idx, elem in enumerate(reversed(numbers), start=1):
+        print(heap_arr)
+        assert elem == heap.pop(heap_arr), f"Broke at {idx}/{len(numbers)} elements"
