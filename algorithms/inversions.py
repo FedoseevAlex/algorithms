f36dd4fcf23c96754456d0edffbb59e14844ba17
Fedoseev Aleksandr <fedoseev@selectel.ru>
2020-04-06 16:49:51 +0300

    Working inversions code

--- algorithms/inversions.py
+++ algorithms/inversions.py
@@ -1,22 +1,22 @@
 from typing import List
 
 
-def merge_sort(array: List[int]) -> List[int]:
-    if len(array) > 2:
+def get_inversions(array: List[int]) -> List[int]:
+    if len(array) >= 2:
         mid = len(array) // 2
-        left = merge_sort(array[:mid])
-        right = merge_sort(array[mid:])
-        return merge(left, right)
-    elif len(array) == 2:
-        return [min(array), max(array)]
-    else:
-        return array
+        left_inversions, left = get_inversions(array[:mid])
+        right_inversions, right = get_inversions(array[mid:])
+        merged_inversions, merged = merge(left, right)
+        return merged_inversions + left_inversions + right_inversions, merged
+    return 0, array
 
 
 def merge(first: List[int], second: List[int]) -> List[int]:
     result = list()
+    inversions = 0
 
     while True:
+        print(first, second)
         if not first and not second:
             break
 
@@ -26,12 +26,11 @@ def merge(first: List[int], second: List[int]) -> List[int]:
         elif not second:
             result.extend(first)
             first.clear()
-        elif first[0] < second[0]:
+        elif first[0] <= second[0]:
             result.append(first.pop(0))
         else:
+            inversions += len(first)
             result.append(second.pop(0))
+        print(inversions)
 
-    return result
-
-def get_inversions(array):
-    pass
+    return inversions, result
--- tests/inversions_test.py
+++ tests/inversions_test.py
@@ -2,10 +2,11 @@ import pytest
 
 from algorithms import inversions as inv
 
-test_data = [
-    ([2, 3, 9, 2, 9], 2)
-]
+test_data = [([1, 2, 3, 5, 4], 1), ([2, 3, 9, 2, 9], 2), ([7, 6, 5, 4, 3, 2, 1], 21)]
+
 
 @pytest.mark.parametrize("array,control", test_data)
 def test_inversions(array, control):
-    assert inv.get_inversions(array) == control
+    inversions, res = inv.get_inversions(array)
+    print(res)
+    assert inversions == control
